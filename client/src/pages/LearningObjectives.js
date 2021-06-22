c4d8d1f9b6d530030579db7c48d7a25475d2f478
remidoukmak <73621087+remidoukmak@users.noreply.github.com>
2021-06-22 09:58:43 +0100

    Add files via upload

---
+++ client/src/pages/LearningObjectives.js
@@ -0,0 +1,57 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// // function displayLearningObjectives()
+
+// const LearningObjectives = () => {
+// 	return (
+// 		<div>
+// 			<div>
+// 				<button id="html-css">HTML/CSS</button>
+// 				<button id="javaScript">JavaScript</button>
+// 				<button id="react">React</button>
+// 			</div>
+// 			<div id="html-css">
+// 				<h2>HTML/CSS</h2>
+// 				<div>
+// 					<p>Understand what 'parent' and 'child' is </p>
+// 					<button>Not Yet Learned</button>
+// 					<button>In Progress</button>
+// 					<button>Support Needed</button>
+// 					<button>Mastered</button>
+// 				</div>
+// 			</div>
+// 			<div id="javaScript">
+// 				<h2>JavaScript</h2>
+// 				<div>
+// 					<p>Understand what 'parent' and 'child' is</p>
+// 					<button>Not Yet Learned</button>
+// 					<button>In Progress</button>
+// 					<button>Support Needed</button>
+// 					<button>Mastered</button>
+// 				</div>
+// 			</div>
+// 			<div id="react">
+// 				<h2>React</h2>
+// 				<div>
+// 					<p>Understand what 'parent' and 'child' is</p>
+// 					<p>Understand what 'parent' and 'child' is</p>
+// 					<button>Not Yet Learned</button>
+// 					<button>In Progress</button>
+// 					<button>Support Needed</button>
+// 					<button>Mastered</button>
+// 				</div>
+// 			</div>
+// 			<div>
+// 				<button type="submit">Log In</button>
+// 				<button>
+// 					<Link className="link" to="/">
+//             Cancel
+// 					</Link>
+// 				</button>
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+// export default LearningObjectives;
