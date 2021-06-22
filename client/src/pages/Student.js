57bed45b285a7784a7459d3a0db0d1663742124b
remidoukmak <73621087+remidoukmak@users.noreply.github.com>
2021-06-22 09:59:24 +0100

    Add files via upload

--- client/src/pages/Student.js
+++ client/src/pages/Student.js
@@ -1,44 +1,49 @@
-import React from 'react';
+// import React from "react";
 
 
-const Student = () => {
-    return (
-        <div>
-            <h1>Name</h1>
-            <div>
-                <button id="html-css">HTML/CSS</button>
-                <button id="javaScript">JavaScript</button>
-                <button id="react">React</button>
-            </div>
-            <div id="html-css">
-                <h2>HTML/CSS</h2>
-                <div>
-                    <p>Understand what 'parent' and 'child' is </p>
-                    <button>Not Confident</button>
-                    <button>Needs Revision</button>
-                    <button>Confident</button>
-                </div>
-            </div>
-            <div id="javaScript">
-                <h2>JavaScript</h2>
-                <div>
-                    <p>Understand what 'parent' and 'child' is</p>
-                    <button>Not Confident</button>
-                    <button>Needs Revision</button>
-                    <button>Confident</button>
-                </div>
-            </div>
-            <div id="react">
-                <h2>React</h2>
-                <div>
-                    <p>Understand what 'parent' and 'child' is</p>
-                    <button>Not Confident</button>
-                    <button>Needs Revision</button>
-                    <button>Confident</button>
-                </div>
-            </div>
-        </div>
-    )
-}
 
-export default Student;
+// const Student = () => {
+// 	return (
+// 		<div>
+// 			<h1>Name</h1>
+// 			<div>
+// 				<button id="html-css">HTML/CSS</button>
+// 				<button id="javaScript">JavaScript</button>
+// 				<button id="react">React</button>
+// 			</div>
+// 			<div id="html-css">
+// 				<h2>HTML/CSS</h2>
+// 				<div>
+// 					<p>Understand what 'parent' and 'child' is </p>
+// 					<button>Not Yet Learned</button>
+// 					<button>In Progress</button>
+// 					<button>Support Needed</button>
+// 					<button>Mastered</button>
+// 				</div>
+// 			</div>
+// 			<div id="javaScript">
+// 				<h2>JavaScript</h2>
+// 				<div>
+// 					<p>Understand what 'parent' and 'child' is</p>
+// 					<button>Not Yet Learned</button>
+// 					<button>In Progress</button>
+// 					<button>Support Needed</button>
+// 					<button>Mastered</button>
+// 				</div>
+// 			</div>
+// 			<div id="react">
+// 				<h2>React</h2>
+// 				<div>
+// 					<p>Understand what 'parent' and 'child' is</p>
+// 					<p>Understand what 'parent' and 'child' is</p>
+// 					<button>Not Yet Learned</button>
+// 					<button>In Progress</button>
+// 					<button>Support Needed</button>
+// 					<button>Mastered</button>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+// export default Student;
