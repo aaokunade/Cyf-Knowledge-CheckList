1d4629cfc879e6a8e25432c2f29dfa0ab055541b
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-06-25 23:12:27 +0100

    useEffect and useState added

--- client/src/pages/Student.js
+++ client/src/pages/Student.js
@@ -1,56 +1,54 @@
-import React from 'react';
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable react/jsx-key */
+import React, {useEffect, useState} from 'react';
 import { Link } from "react-router-dom";
 import Footer from './Footer';
 
 
 const Student = () => {
-    return (
-        <div className="student-page">
-            <div>
-                <h1>CODE YOUR FUTURE</h1>
-            </div>
-            <div className="student-header">
-                <h2>Name</h2>
-                <button><Link className="link" to="/">
+	const [lessons, setLessons] = useState({})
+	useEffect(() => {
+		fetch('api/studentsPage')
+			.then((result) => result.json())
+			.then((lessons) => {
+				console.log(lessons);
+				setLessons(lessons)
+			})
+	}, [])
+	return (
+		<div className="student-page">
+			<div>
+				<h1>CODE YOUR FUTURE</h1>
+			</div>
+			<div className="student-header">
+				<h2>Name</h2>
+				<button><Link className="link" to="/">
                   Log out
-                </Link></button>
-            </div>
+				</Link></button>
+			</div>
             
-            <div className="skills-btn-container">
-                <button className="skills-btn" ><a href="#html-css" >HTML/CSS</a></button>
-                <button className="skills-btn" ><a href="#javaScript">JavaScript</a></button>
-                <button className="skills-btn" ><a href="#react">React</a></button>
-            </div>
-            <div className="skill-section" id="html-css">
-                <h2>HTML/CSS</h2>
-                <div className="competency-level">
-                    <p>Understand what 'parent' and 'child' is </p>
-                    <button>Not Confident</button>
-                    <button>Needs Revision</button>
-                    <button>Confident</button>
-                </div>
-            </div>
-            <div className="skill-section" id="javaScript">
-                <h2>JavaScript</h2>
-                <div className="competency-level">
-                    <p>Understand what 'parent' and 'child' is</p>
-                    <button>Not Confident</button>
-                    <button>Needs Revision</button>
-                    <button>Confident</button>
-                </div>
-            </div>
-            <div className="skill-section" id="react">
-                <h2>React</h2>
-                <div className="competency-level">
-                    <p>Understand what 'parent' and 'child' is</p>
-                    <button>Not Confident</button>
-                    <button>Needs Revision</button>
-                    <button>Confident</button>
-                </div>
-            </div>
-             <Footer />
-        </div>
-    )
+			<div className="skills-btn-container">
+				{Object.keys(lessons).map((lesson, index) => (
+					<button className="skills-btn"  key = {index}><a href="#html-css" >{lesson}</a></button>
+				))}
+			</div>
+			{Object.keys(lessons).map((lesson, index) => (
+				<div className="skill-section" id="html-css"  key = {index}>
+					<h2>{lesson}</h2>
+					{lessons[lesson].map((obj, index) =>(
+						<div className="competency-level"  key = {index}>
+							<p>{obj}</p>
+							<button>Not Confident</button>
+							<button>Needs Revision</button>
+							<button>Confident</button>
+						</div>
+					))}
+					
+				</div>
+			))}
+			<Footer />
+		</div>
+	)
 }
 
 export default Student;
