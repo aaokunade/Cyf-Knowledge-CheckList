2a216295b01d4b2b0208e2735ba17e5d91a675d5
Miss <emesquitta73@gmail.com>
2021-07-05 19:16:29 +0100

    added mentor page and css

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,16 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import LogIn from "./pages/LogIn";
 import SignUp from "./pages/SignUp";
-import IncorrectUrlError from './IncorrectUrlError';
-import Student from './pages/Student';
+import IncorrectUrlError from "./IncorrectUrlError";
+import Student from "./pages/Student";
+import Mentor from  "./pages/Mentor"
 
 
 const App = () => (
 	<div>
 	 <Switch>
-	 	<Route path="/Student"><Student /></Route>
+		 <Route path="/Mentor"><Mentor /></Route>
+	 	 <Route path="/Student"><Student /></Route>
 		 <Route path="/" exact><Home /></Route>
 		 <Route path="/LogIn"><LogIn /></Route>
 		 <Route path="/SignUp"><SignUp /></Route>
