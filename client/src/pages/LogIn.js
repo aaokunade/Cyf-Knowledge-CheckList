ed1d753e5af82c4c90120ef9045b5248b68a8a5a
JK1320 <kaurjus1@gmail.com>
2021-07-05 22:40:12 +0100

    url upper to lower case & removed unused state

--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -9,7 +9,6 @@ import logo from "./Images/cyf_logo.jpeg";
 function LogIn() {
 const [loginValues, setLoginValues] = useState({email: "", password: ""})
 const [errors, setErrors] = useState({});
-// const [loginStatus, setLoginStatus] = useState();
 const [isLogged, setIsLogged] = useState(false);
 const [details, setDetails] = useState({errorMessage: "", userId: "", userName: ""});
 
@@ -38,7 +37,6 @@ const handleLogChange = (e) => {
        if(res.message === "wrong password" || res.message === "cannot find user"){
         setIsLogged(false);
         setDetails({errorMessage:res.message, userId:""})
-        // setLoginStatus();
       } else if(res.message === 2) {
         setIsLogged(true);
         setDetails({errorMessage:"", userId:res.id, userName: res.name})
@@ -101,8 +99,7 @@ const handleLogChange = (e) => {
                 </div>
               </form>
             </div>
-            {/* <h2>{loginStatus}</h2> */}
-            <p className="login-signup-link">Don't have an account. <Link className="link login-signup" to="/SignUp">
+            <p className="login-signup-link">Don't have an account. <Link className="link login-signup" to="/sign-up">
               Sign Up
             </Link></p>
             <Footer />
