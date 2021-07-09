3004d97d01ac93bc7bf91b7bca508f8cd54ebdae
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-07-09 12:33:03 +0100

    Added theAdd  new objectives route and its states and variables

--- client/src/pages/Mentor.js
+++ client/src/pages/Mentor.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-onchange */
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
@@ -6,71 +7,109 @@ import { globalState } from "./LogIn";
 
 
 //all useStates- to collate all students and lessons from the dropdown selection box
-  const Mentor = (props) => {
-  const [lessons, setLessons] = useState({});
-  const [lessonsDropDown, setLessonsDropDown] = useState([]);
-  const [updateCompetency, setUpdateCompetency] = useState({
-    lesson: "",
-    obj_id: 0,
-    competencyId: 0,
-    user: {},
-  });
-  const [competency, setCompetency] = useState([]);
-  const [students, setStudents] = useState([]);
-  const handleChange = (event) => {
-    fetch("api/competency")
-      .then((result) => result.json())
-      .then((competency) => {
-        setCompetency(competency);
-        fetch("api/students-page", {
-          method: "POST",
-          headers: {
-            Authorization: `Token ${globalState.token}`,
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ userId: props.user.userId }),
-        })
-          .then((result) => result.json())
-          .then((lessons) => {
-            setLessons(lessons.lessons);
-            console.log(lessons)
-          });
-      });
-  // useEffect - this token recogonises the Student account selected & provides access to their Overview page
-  };
-  useEffect(() => {
-    fetch("api/getstudentsonly", {
-      method: "POST",
-      headers: {
-        Authorization: `Token ${globalState.token}`,
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ userId: props.user.userId }),
-    })
-      .then((result) => result.json())
-      .then((students) => {
-        setStudents(students);
-      });
-      fetch("api/lessons")
-       .then((result) => result.json())
-      .then((students) => {
-        setLessonsDropDown(students);
-      });
-  }, [props]);
-  return (
-    <>
-      <div className="header">
-        <div className="logo-image">
-          <img className="image" src={logo} alt="cyf_logo" />
-        </div>
-        <h2>Mentor Overview</h2>
-      </div>
-      <div className="Mentor-Name">
-        <h2>{props.user.userName}</h2>
-        <div className="Mlogout">
-          <Link className="mentor-logout-link" to="/">
+const Mentor = (props) => {
+	const [lessons, setLessons] = useState({});
+	const [lessonsDropDown, setLessonsDropDown] = useState([]);
+	const [updateCompetency, setUpdateCompetency] = useState({
+		lesson: "",
+		obj_id: 0,
+		competencyId: 0,
+		user: {},
+	});
+	const [addObj, setAddObj] = useState({
+		newObj:"",
+		lesson:"",
+	});
+	const [isSubmitted, setIsSubmitted] = useState(false);
+	const [competency, setCompetency] = useState([]);
+	const [students, setStudents] = useState([]);
+	const handleChange = (event) => {
+		fetch("api/competency")
+			.then((result) => result.json())
+			.then((competency) => {
+				setCompetency(competency);
+				fetch("api/students-page", {
+					method: "POST",
+					headers: {
+						Authorization: `Token ${globalState.token}`,
+						Accept: "application/json",
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ userId: props.user.userId }),
+				})
+					.then((result) => result.json())
+					.then((lessons) => {
+						setLessons(lessons.lessons);
+						console.log(lessons);
+					});
+			});
+		// useEffect - this token recogonises the Student account selected & provides access to their Overview page
+	};
+	useEffect(() => {
+		fetch("api/get-students-only", {
+			method: "POST",
+			headers: {
+				Authorization: `Token ${globalState.token}`,
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ userId: props.user.userId }),
+		})
+			.then((result) => result.json())
+			.then((students) => {
+				setStudents(students);
+			});
+		fetch("api/lessons")
+			.then((result) => result.json())
+			.then((students) => {
+				setLessonsDropDown(students);
+			});
+	}, [props]);
+
+	const handleObjAdd = (event) => {
+		const { name, value } = event.target;
+		setAddObj({
+			...addObj,
+			[name]:value,
+		});
+	};
+
+	const handleObjSubmit = (event) => {
+		event.preventDefault();
+		fetch("/api/objectives", {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(addObj),
+		})
+			.then((result) => result.json())
+			.then((res) => {
+				console.log(res);
+				if (res.message === "New LearningObjectives inserted!") {
+					setIsSubmitted(true);
+					alert("New Objective Added");
+				} else {
+					setIsSubmitted(false);
+					alert("Check fields are entered correctly");
+				}
+			});
+	};
+
+
+	return (
+		<>
+			<div className="header">
+				<div className="logo-image">
+					<img className="image" src={logo} alt="cyf_logo" />
+				</div>
+				<h2>Mentor Overview</h2>
+			</div>
+			<div className="Mentor-Name">
+				<h2>{props.user.userName}</h2>
+				<div className="Mlogout">
+					<Link className="mentor-logout-link" to="/">
 
             Log out
 					</Link>
@@ -90,7 +129,7 @@ import { globalState } from "./LogIn";
 				</select>
 			</div>
 			<div className="mentor-sentence">
-				<label className="Objtitle" htmlFor="fname">
+				<label className="Objtitle" htmlFor="newObj">
 					<h2>New Objectives</h2>
 				</label>
 				<h3>
@@ -105,33 +144,37 @@ import { globalState } from "./LogIn";
 						<input
 							type="text"
 							id="NewObj"
+							onChange={handleObjAdd}
 							className="new-obj"
-							name="fname"
+							name="newObj"
+							value={addObj.newObj}
 							placeholder="Insert new objectives here"
 						></input>
-						<div className="lesson-dropdown">
-							<select className="lesson-select" name="select">
-								<option className="option">Select Lesson</option>
-								{lessonsDropDown.map((lesson, index) => (
-									<option key={index} value={lesson}>
-										{lesson.lessons}
-									</option>
-								))}
-							</select>
-						</div>
+						{/* {errors.lesson && <p className="error">{errors.lesson}</p>} */}
 					</div>
-					<input
+					<div className="lesson-dropdown">
+						<select className="lesson-select" name="lesson" onChange={handleObjAdd}>
+							<option className="option">Select Lesson</option>
+							{lessonsDropDown.map((lesson, index) => (
+								<option key={index} value={lesson.lessons}>
+									{lesson.lessons}
+								</option>
+							))}
+						</select>
+						{/* {errors.lesson && <p className="error">{errors.lesson}</p>} */}
+					</div>
+
+					<button
 						className="submitbutn"
 						type="submit"
-						formTarget="_blank"
-						value="Submit New Objective"
-					></input>
-					<input
+						onClick={handleObjSubmit}
+						// formTarget="_blank"
+					>Submit New Objective</button>
+					<button
 						className="deletebutn"
 						type="submit"
-						formTarget="_blank"
-						value="Delete Objective"
-					></input>
+						// formTarget="_blank"
+					>Delete Objective</button>
 				</form>
 			</div>
 			<div className="skills-btn-container">
